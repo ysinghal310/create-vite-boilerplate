13f352b45ae8094949dcf8596262023ff776a340
ysinghal310 <gangbuster74@gmail.com>
2023-10-28 11:29:38 +0530

    dfdfdfdf

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve("src", "src/main.tsx"),
+      name: "create-dev-vite",
+      fileName: (format) => `create-dev-vite.${format}.ts`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
   plugins: [react()],
 });
